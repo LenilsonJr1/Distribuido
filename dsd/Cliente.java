51d527afa7b94490709905c2fe90701ded14ac29
Lenilson Tomaz <47852839+LenilsonJr1@users.noreply.github.com>
2019-09-12 09:12:18 -0300

    Add files via upload

---
+++ dsd/Cliente.java
@@ -0,0 +1,28 @@
+package dsd;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.io.IOException;
+public class Cliente {
+ static byte[] mensagem = "Num ninho de mafagafos há sete mafagafinhos. Quando a mafagafa gafa, gafam os sete mafagafinhos.".getBytes();
+ static int porta = 8181;
+	 public static void main(String a[]){
+		 try{
+			 
+			 DatagramSocket soquete = new DatagramSocket();
+			 DatagramPacket dp = new DatagramPacket(mensagem, mensagem.length,InetAddress.getByName("192.168.0.6"), porta);
+			 System.out.println(dp.getData().length);
+			 soquete.send(dp);
+			 soquete.close();
+			 
+		 }catch(SocketException se){
+			 se.printStackTrace();
+		 }catch(UnknownHostException uhe){
+			 uhe.printStackTrace();
+		 }catch(IOException ioe){
+			 ioe.printStackTrace();
+		 }
+	 }
+}
---
+++ dsd/Servidor.java
@@ -0,0 +1,37 @@
+package dsd;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.DatagramPacket;
+import java.io.IOException;
+import java.net.InetAddress;
+public class Servidor {
+	
+ static int porta = 8181;
+ 
+	 public static void main(String a[]){
+		 try{
+			 DatagramSocket soquete = new DatagramSocket(porta);
+			
+			 byte buffer[] = new byte[85536];
+			 DatagramPacket datagrama = new DatagramPacket(buffer, buffer.length);
+			 System.out.println("Aguardando solicitacoes");
+			
+			 long start = System.currentTimeMillis();
+			 soquete.receive(datagrama);
+			 
+			 System.out.println("A mensagem recebida é : " + (new String(datagrama.getData()).trim()) + 
+					 " , o tamanho é : " + datagrama.getLength());
+			 
+			 soquete.close();
+			 long fim = System.currentTimeMillis() ;
+			 long tempoDif =   fim -  start;
+			 
+			 System.out.println("E o tempo gasto foi : " + tempoDif /  1000 + " Segundos");
+			 
+		 }catch(SocketException se){
+			 se.printStackTrace();
+		 }catch(IOException ioe){
+			 ioe.printStackTrace();
+		 }
+	 }
+}
